4aeddc908d10db564a0fa1a268313659ce2e6119
Kent <kent05011607@gmail.com>
2021-07-27 22:54:15 +0800

    removed unused sudo command

--- ta_bot/cogs/utils.py
+++ ta_bot/cogs/utils.py
@@ -9,20 +9,7 @@ class UtilsCog(commands.Cog, name='Utils'):
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def sudo(self,
-                   ctx: commands.Context,
-                   user: discord.Member,
-                   *,
-                   command: str):
-        '''Execute a command as another user'''
-        new_message = copy(ctx.message)
-        new_message.author = user
-        new_message.content = ctx.prefix + command
-        await self.bot.process_commands(new_message)
-
+        
     @commands.command(brief='Pings the bot')
     async def ping(self, ctx):
         '''Get the latency of the bot'''
